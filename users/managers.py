503245f928e8a19088532a6a9276bb4301eaeb56
Mohamad-Abodaa <m.abdelhamid@siaforce.net>
2024-02-21 01:37:23 +0200

    update users managers to use usrename instead of password

--- users/managers.py
+++ users/managers.py
@@ -21,12 +21,12 @@ class UserManager(BaseUserManager):
         except:
             raise
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self._create_user(email, password=password, **extra_fields)
+        return self._create_user(username, password=password, **extra_fields)
