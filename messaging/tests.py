55db8c9892b343c9d564548a8783d4f9fcccadd7
Mohamad-Abodaa <m.abdelhamid@siaforce.net>
2024-02-21 01:34:57 +0200

    Added some tests for users and messaging app

--- messaging/tests.py
+++ messaging/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+import pytest
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import UserMessage, Message
+
+User = get_user_model()
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def user_data():
+    return {
+        'username': 'testuser',
+        'password': 'testpassword',
+    }
+
+@pytest.fixture
+def message_data():
+    return {
+        'text': 'Hello, testing!',
+        'receiver_username': 'receiveruser',
+    }
+
+@pytest.mark.django_db
+def test_send_message(api_client, user_data, message_data):
+    sender = User.objects.create_user(**user_data)
+    api_client.force_authenticate(user=sender)
+
+    receiver = User.objects.create_user(username=message_data['receiver_username'], password='receiverpassword')
+
+    response = api_client.post(reverse('messaging:send_message'), message_data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_get_outbox(api_client, user_data, message_data):
+    sender = User.objects.create_user(**user_data)
+    api_client.force_authenticate(user=sender)
+
+    
+
+    # Send a message
+    _ = User.objects.create_user(username=message_data['receiver_username'], password='receiverpassword')
+    api_client.post(reverse('messaging:send_message'), message_data, format='json')
+
+    response = api_client.get(reverse('messaging:outbox_message'))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 1 
+
+
+
+@pytest.mark.django_db
+def test_get_inbox(api_client, user_data, message_data):
+    sender = User.objects.create_user(**user_data)
+    api_client.force_authenticate(user=sender)
+
+    receiver = User.objects.create_user(username=message_data['receiver_username'], password='receiverpassword')
+    api_client.post(reverse('messaging:send_message'), message_data, format='json')
+
+    # Retrieve inbox messages for the receiver
+    api_client.force_authenticate(user=receiver)
+    response = api_client.get(reverse('messaging:inbox_message'))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) == 1
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+import pytest
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def user_data():
+    return {
+        'username': 'testuser',
+        'password': 'testpassword',
+    }
+
+@pytest.mark.django_db
+def test_create_user(api_client, user_data):
+    response = api_client.post(reverse('users:register-user'), user_data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+
+@pytest.mark.django_db
+def test_retrieve_user(api_client, user_data):
+    user = User.objects.create_user(**user_data)
+    api_client.force_authenticate(user=user)
+
+    response = api_client.get(reverse('users:list-update'))
+    assert response.status_code == status.HTTP_200_OK
+
+@pytest.mark.django_db
+def test_update_user(api_client, user_data):
+    user = User.objects.create_user(**user_data)
+    api_client.force_authenticate(user=user)
+
+    updated_data = {
+        'username':'testuser2',
+        'password': 'updatepassword'
+    }
+
+    response = api_client.put(reverse('users:list-update'), updated_data, format='json')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['username'] == updated_data['username']
+
+@pytest.mark.django_db
+def test_partial_update_user(api_client, user_data):
+    user = User.objects.create_user(**user_data)
+    api_client.force_authenticate(user=user)
+
+    updated_data = {
+        'username': 'updated username',
+    }
+
+    response = api_client.patch(reverse('users:list-update'), updated_data, format='json')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['username'] == updated_data['username']
