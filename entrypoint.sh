36dacdec04a11779fd9808d9b2b73d7461906ef3
Mohamad-Abodaa <m.abdelhamid@siaforce.net>
2024-02-21 00:10:49 +0200

    Add entrypoint.sh for Docker container entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Apply migrations
+python manage.py migrate
+
+# Create new migration files
+python manage.py makemigrations
+
+# Start the Django development server
+python manage.py runserver 0.0.0.0:8000
